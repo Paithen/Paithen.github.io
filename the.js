43691742920b602d19f06f5dfce9a4ed1ce30369
Paithen <samikharouf@gmail.com>
2020-09-04 14:14:52 +0300

    Update the.js

--- the.js
+++ the.js
@@ -1,15 +1,1 @@
-$(function(){
-	function AYUWAWA(){
 
-
-		alert("AYUWAWA");
-		while(true){
-			var y = "AYUWAWA";
-			var x = x + y; 
-			$("p").html(x);	
-		}
-
-	}
-	
-	$("button.the").on("click",(function(){AYUWAWA();});
-});

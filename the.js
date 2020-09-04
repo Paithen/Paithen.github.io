c487a584d348b655ca463b9fc7823437a5fd0e7c
Paithen <samikharouf@gmail.com>
2020-09-04 17:43:19 +0300

    Update the.js

--- the.js
+++ the.js
@@ -1,9 +1,4 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-	function once(){
-		document.cookie = "clicked=yes"
-		location.reload();
-	}
-
 	if(document.cookie == "clicked=yes"){
 		document.body.innerHTML = '';
 	}
